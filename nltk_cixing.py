54232d721ff32ca87831761b25467b50ed03f6b7
wanchao.gao@adinall.com <wanchao.gao@adinall.com>
2018-11-01 19:53:52 +0800

    句型

--- nltk_cixing.py
+++ nltk_cixing.py
@@ -3,14 +3,11 @@
 import nltk
 import jieba.analyse
 import jieba.posseg
-
-from nltk.corpus import treebank
-t = treebank.parsed_sents('/Users/gaowanchao/a.txt')[0]
-t.draw()
+# jieba.load_userdict('userdict.txt')
 
 s = '金庸逝世！江湖再无金大侠，只剩沧海一声笑……'
 s = '其实大家买手机就是看个心情，没必要比来比去的。'
-s = '其实 大家买手机'
+s = '因为你不好，所以我不喜欢你。'
 
 words = jieba.posseg.cut(s)
 
@@ -22,31 +19,28 @@ for word, f in words:
     l.append(word)
 
 g = '''
-    S -> D VN
+    S -> D NN DVLN X VD L UJ X
+    NN -> N N
+    DVLN -> D V L N
+    VD  -> V D
     D -> '其实' | '就是' | '必要'
-    VN -> NN
     N -> '大家' | '买手机' | '心情'
-    v -> '看' | '没'
-    l -> '个' | '比来比去'
+    V -> '看' | '没'
+    L -> '个' | '比来比去'
+    X -> '，' | '。'
+    UJ -> '的'
+'''
+
+g = '''
+    s -> c rd x c rdvr x
+    rd -> r d
+    rdvr -> r d v r
+    c -> '因为' | '所以'
+    d -> '不' | '不好'
+    r -> '我' | '你'
+    v -> '喜欢'
     x -> '，' | '。'
-    uj -> '的'
 '''
-
-
-g1 = '''
-    S -> NP VP
-    VP -> V NP | V NP PP
-    PP -> P NP
-    V -> "saw" | "ate" | "walked" | '爱'
-    NP -> "John" | "Mary" | "Bob" | Det N | Det N PP | '我' | '你'
-    Det -> "a" | "an" | "the" | "my"
-    N -> "man" | "dog" | "cat" | "telescope" | "park"
-    P -> "in" | "on" | "by" | "with"
-'''
-
-
-s1 = '我 爱 你'
-l1 = list(s.split())
 
 grammar1 = nltk.CFG.fromstring(g)
 
@@ -54,5 +48,17 @@ rd_parser = nltk.RecursiveDescentParser(grammar1)
 
 for tree in rd_parser.parse(l):
     print(tree)
- 
-tree.draw()
+    print ('\n'.join(dir(tree)))
+
+# print (tree['c'])
+# tree.reverse()
+# tree.sort()
+
+ge = nltk.parse.generate(grammar1)
+print (ge.generate())
+
+
+print ('-----------------------------')
+for t in tree:
+    print (t.label())
+# tree.draw()
